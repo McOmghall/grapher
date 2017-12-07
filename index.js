b1be51f1e499571a254672e82d28ec41dbebc103
McOmghall <pmgberm@gmail.com>
2017-12-07 20:53:18 +0100

    Added deepness parameter to crawl function

--- index.js
+++ index.js
@@ -11,30 +11,32 @@ const isInsideWikiPages = (e) => INSIDE_WIKI_PAGES.test(e)
 const isNotASpecialPage = (e) => !SPECIAL_PAGES.some((sp) => sp.test(e))
 const filterWikiURL = (e) => isInsideWikiPages(e) && isNotASpecialPage(e)
 
+const crawlWithDeepness = function (deepness, error, res, done) {
+  if (error) {
+    console.error('D:%d ERRORED %s -> %s', deepness, res.options.uri, null);
+  } else {
+    console.log('D:%d SUCCESS %s %s -> %s', deepness, res.request.method, res.request.uri.href, res.$('title').text());
+
+    const links = res.$('a')
+    console.log('D:%d SUCCESS %s %s -> Parsing %s links', deepness, res.request.method, res.request.uri.href, links.length)
+
+    const filterLinks = links.get().map((e) => res.$(e).attr('href')).filter(filterWikiURL).map((e) => URL.resolve(res.request.uri.href, e))
+    const uniqueHrefs = Array.from(new Set(filterLinks))
+
+    console.log('D:%d SUCCESS %s %s -> Got %s valid hrefs', deepness, res.request.method, res.request.uri.href, uniqueHrefs.length)
+
+    c.queue(uniqueHrefs.map((e) => ({ uri: e, callback: crawlWithDeepness.bind(this, deepness + 1)})))
+  }
+  done();
+}
+
 const c = new Crawler({
   maxConnections: 10,
   preRequest: function (opts, done) {
     done()
   },
   // This will be called for each crawled page
-  callback: function (error, res, done) {
-    if (error) {
-      console.error('ERRORED %s -> %s', res.options.uri, null);
-    } else {
-      console.log('SUCCESS %s %s -> %s', res.request.method, res.request.uri.href, res.$('title').text());
-
-      const links = res.$('a')
-      console.log('SUCCESS %s %s -> Parsing %s links', res.request.method, res.request.uri.href, links.length)
-
-      const filterLinks = links.get().map((e) => res.$(e).attr('href')).filter(filterWikiURL).map((e) => URL.resolve(res.request.uri.href, e))
-      const uniqueHrefs = Array.from(new Set(filterLinks))
-
-      console.log('SUCCESS %s %s -> Got %s valid hrefs', res.request.method, res.request.uri.href, uniqueHrefs.length)
-
-      c.queue(uniqueHrefs)
-    }
-    done();
-  }
+  callback: crawlWithDeepness.bind(this, 0)
 });
 
 
